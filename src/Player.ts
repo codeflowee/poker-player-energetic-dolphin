0b2d315a6dc51e725fa53ebdf68a730f28121581
Andre Toodre <toodre@rushstreetinteractive.com>
2023-11-30 10:42:47 +0200

    Place player stack

--- src/Player.ts
+++ src/Player.ts
@@ -3,7 +3,12 @@ import { GameState } from "./interfaces/GameState";
 export class Player {
   public betRequest(gameState: GameState, betCallback: (bet: number) => void): void {
     console.log('betRequest gameState', gameState);
-    betCallback(1000);
+
+    const player = gameState.players.find(({ name }) => name === 'Energetic Dolphin');
+
+    if (player) {
+      betCallback(player.stack);
+    }
   }
 
   public showdown(gameState: GameState): void {
