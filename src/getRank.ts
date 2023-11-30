3cd2082ff1550e1f3cedffacc9230d61473382b0
Andre Toodre <toodre@rushstreetinteractive.com>
2023-11-30 16:29:03 +0200

    Update

--- src/getRank.ts
+++ src/getRank.ts
@@ -4,23 +4,23 @@ import axios from 'axios';
 async function getRank(gameState: GameState) {
   const player = gameState.players[gameState.in_action];
 
-  const playerCards = player.hole_cards;
+  const playerCards = player.hole_cards ?? [];
   const tableCards = gameState.community_cards;
 
-  if (playerCards && tableCards) {
+  if (playerCards.length && tableCards.length) {
     console.log('getRank data', {
       playerCards,
       tableCards,
     })
 
     try {
-      const response = await axios.get('https://rainman.leanpoker.org/rank', {
+      const { data } = await axios.get('https://rainman.leanpoker.org/rank', {
         params: {
           cards: JSON.stringify([...playerCards, ...tableCards])
         }
       })
 
-      console.log('getRank response', response)
+      console.log('getRank response', data)
     } catch (e) {
       console.log('getRank error', e)
     }
